e718c96751bf34a9bd27b8a9dac8039159946b15
yena <spffhd0835@gmail.com>
2024-06-25 23:40:13 +0900

    study: 파일업로드 mvc로 변경하기 위한 세팅

---
+++ 24-file_upload/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+
+app.listen(PORT,()=>{
+  console.log(`http://localhost:${PORT}`);
+});
