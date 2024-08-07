0a0f7ff9446ea362d3414dca6fbea414e6191831
yenaf <spffhd0835@gmail.com>
2024-08-07 14:32:50 +0900

    study : 종합실습 학습완료

--- 39-event-practice/src/App.js
+++ 39-event-practice/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
-import Practice from './Practice';
+// import Practice from './Practice';
+import Prac1 from './Prac1';
+// import Prac2 from './Prac2';
 
 function App() {
   return (
     <div className="App">
-      <Practice/>
+      {/* <Practice/> */}
+      {/* <Prac2/> */}
+      <Prac1/>
     </div>
   );
 }
